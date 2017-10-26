8fc0fe293b4e58ac09e873a00a073efe01486da7
Jungsoo Park <jungsoopark96@gmail.com>
2017-10-26 17:57:42 -0400

    SQUASH

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -8,10 +8,19 @@ public class Client {
     String hostname = input.nextLine();
     System.out.print("Port: ");
     String port = input.nextLine();
-    String protocol = "";
-    while (!protocol.equalsIgnoreCase("udp") && !protocol.equalsIgnoreCase("tcp")) {
-      System.out.print("Transport Protocol (TCP/UDP): ");
-      protocol = input.nextLine();
+    String transport = "";
+    while (!transport.equals("1") && !transport.equals("2")) {
+      System.out.print("Transport Protocol [(1) UDP (2) TCP]: ");
+      transport = input.nextLine();
     }
+    String acknowledgement = "";
+    while (!acknowledgement.equals("1") && !acknowledgement.equals("2")) {
+      System.out.print("Acknowledgement Protocol [(1) Streaming, (2) Stop-and-wait]: ");
+      acknowledgement = input.nextLine();
+    }
+    System.out.print("Message size [2^n, 0 <= n <= 16]: ");
+    String messageSize = input.nextLine();
+
+    System.out.println(String.format("%s:%s -- %s -- %s -- %s", hostname, port, transport, acknowledgement, messageSize));
   }
 }
