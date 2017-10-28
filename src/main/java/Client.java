4958f1de82845fa72cad7c547d9e51100af35124
Jungsoo Park <jungsoopark96@gmail.com>
2017-10-28 15:09:31 -0400

    UDP client, didn't test

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,7 +1,10 @@
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.io.IOException;
+import java.net.*;
 
 public class Client {
+
+  private static final long TOTAL_SIZE = 10;
+  private static final Integer ACK_BYTE = 255;
 
   public enum TransportProtocol {
     TCP, UDP
@@ -38,7 +41,53 @@ public class Client {
     }
   }
 
-  private static void runUdp(InetAddress address, int port, AckProtocol ackProtocol, int messageSize) {
+  private static void runUdp(InetAddress address,
+                             int port,
+                             AckProtocol ackProtocol,
+                             Integer messageSize) throws IOException {
+
+    DatagramSocket socket = new DatagramSocket(port);
+    byte[] applicationMessage = new byte[3];
+    applicationMessage[0] = messageSize.byteValue();
+    applicationMessage[1] = Integer.valueOf(ackProtocol ==  AckProtocol.STOPANDWAIT ? 1 : 0).byteValue();
+    applicationMessage[2] = Integer.valueOf(applicationMessage[0] + applicationMessage[1]).byteValue();
+
+    DatagramPacket applicationMessagePacket = new DatagramPacket(
+        applicationMessage,
+        0,
+        applicationMessage.length,
+        address,
+        port);
+    DatagramPacket messagePacket = new DatagramPacket(
+        new byte[messageSize],
+        0,
+        messageSize,
+        address,
+        port
+    );
+    DatagramPacket ackPacket = new DatagramPacket(
+        new byte[1],
+        1
+    );
+
+    socket.send(applicationMessagePacket);
+
+    long start = System.currentTimeMillis();
+    long count = TOTAL_SIZE;
+    while (count > 0) {
+      socket.send(messagePacket);
+      count -= messageSize;
+      if (ackProtocol == AckProtocol.STOPANDWAIT) {
+        socket.receive(ackPacket);
+        if (ackPacket.getData()[0] == 255) {
+          continue;
+        } else {
+          System.err.println("Received response, but not ack byte");
+          System.exit(1);
+        }
+      }
+    }
+    System.out.println(System.currentTimeMillis() - start);
   }
 
   private static void runTcp(InetAddress address, int port, AckProtocol ackProtocol, int messageSize) {
